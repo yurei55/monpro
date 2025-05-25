9f27cc2f85a60974f9d2da493aa08ae63eaf9e81
yurei55 <119704216+yurei55@users.noreply.github.com>
2025-05-25 20:50:29 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import FindPage from "./pages/FindPage";
+import JoinPage from "./pages/JoinPage";
+import PaymentPage from "./pages/PaymentPage";
+import ConfirmationPage from "./pages/ComfirmationPage";
+import ReservationPage from "./pages/ReservationPage";
+import ProfilePage from "./pages/ProfilePage";
+import HotelDetailPage from "./pages/HotelDetailPage";
+// ...other imports
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/find"  element={<FindPage />} />
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/comfirmation" element={<ConfirmationPage />} />
+        <Route path="/orders" element={<ReservationPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/hotel/:id" element={<HotelDetailPage />} />
+        {/* ...other routes */}
+      </Routes>
+    </Router>
   );
 }
 
